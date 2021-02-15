c6d1aeaf9f35ae6aa3fbbc388f8d655be7ff325a
ashna111 <ashnashah111@gmail.com>
2021-02-15 17:34:58 +0530

    auth0 as auth service

---
+++ auth0-as-auth-service/README.md
@@ -0,0 +1,7 @@
+### auth0-as-auth-service
+A sample application that uses Auth0 as authentication service
+
+### Steps:
+1. `nvm use`
+2. `npm install`
+3. `npm run start`
---
+++ auth0-as-auth-service/index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express()
+const { auth, requiresAuth } = require('express-openid-connect')
+
+require('dotenv/config')
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: 'a long, randomly-generated string stored in env',
+  baseURL: 'http://localhost:3000',
+  clientID: process.env.CLIENT_ID,
+  issuerBaseURL: process.env.ISSUER_BASE_URL
+}
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config))
+
+// req.isAuthenticated is provided from the auth router
+app.get('/', (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out')
+})
+
+app.get('/profile', requiresAuth(), (req, res) => {
+  res.send(JSON.stringify(req.oidc.user))
+})
+
+app.listen(3000, () => {
+  console.log('Server starting...')
+})
